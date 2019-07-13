8a2fca68568c9cdce09fb38862b0af7d695e5a38
arjun1321 <guptaarjun152@gmail.com>
2019-07-13 23:03:25 +0530

    Cricket API- Project Setup

---
+++ src/main/java/com/arjunkumar/cricketapi/CricketApiApplication.java
@@ -0,0 +1,13 @@
+package com.arjunkumar.cricketapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arjunkumar/cricketapi/CricketApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.arjunkumar.cricketapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CricketApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
