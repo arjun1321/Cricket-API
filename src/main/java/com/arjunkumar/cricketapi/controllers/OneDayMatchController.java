1590c8999919f7f06c627131ee10999f4a32286a
arjun1321 <guptaarjun152@gmail.com>
2019-07-13 23:11:52 +0530

    Added controller classes

---
+++ src/main/java/com/arjunkumar/cricketapi/controllers/OneDayMatchController.java
@@ -0,0 +1,10 @@
+package com.arjunkumar.cricketapi.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OneDayMatchController {
+
+
+}
---
+++ src/main/java/com/arjunkumar/cricketapi/controllers/T20MatchController.java
@@ -0,0 +1,8 @@
+package com.arjunkumar.cricketapi.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class T20MatchController {
+}
