1cc8b0de77e9c8c79601b9f077d30782d37cf925
Arjun Kumar <guptaarjun152@gmail.com>
2019-08-28 23:58:29 +0530

    Successfully retrieved the test team ranking and displayed as json

--- src/main/java/com/arjunkumar/cricketapi/controllers/TestMatchController.java
+++ src/main/java/com/arjunkumar/cricketapi/controllers/TestMatchController.java
@@ -1,10 +1,24 @@
 package com.arjunkumar.cricketapi.controllers;
 
 
+import com.arjunkumar.cricketapi.models.Ranks;
+import com.arjunkumar.cricketapi.services.TestMatchServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/test")
 public class TestMatchController {
 
+    @Autowired
+    TestMatchServiceInterface testMatchService;
+
+    @RequestMapping("/team/rankings")
+    public List<Ranks> getCurrentTestTeamRankings() {
+        return testMatchService.getCurrentTestTeamRankings();
+    }
 
 }
--- src/main/java/com/arjunkumar/cricketapi/crawlers/Crawlers.java
+++ src/main/java/com/arjunkumar/cricketapi/crawlers/Crawlers.java
@@ -57,7 +57,28 @@ public class Crawlers {
     public static List<Ranks> getCurrentTestTeamRankings() {
         List<Ranks> listOfTeams = new ArrayList<>();
 
+        String url = "http://www.espncricinfo.com/rankings/content/page/211271.html";
 
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements elements = doc.select("table");
+
+            Element testTable = elements.get(0);
+            Elements testRows = testTable.select("tr");
+            testRows.remove(0);
+
+            for (int i=1; i<=testRows.size(); i++) {
+                Elements teamData = testRows.get(i-1).select("td");
+
+                Ranks ranks = new Ranks(teamData.get(0).text(), i, teamData.get(3).text(), teamData.get(2).text(),
+                        teamData.get(1).text());
+
+                listOfTeams.add(ranks);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return listOfTeams;
     }
