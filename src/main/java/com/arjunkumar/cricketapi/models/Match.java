5567be22d7291592cee106818e722affaecb3cbc
Arjun Kumar <user@Administrators-MacBook-Pro-2.local>
2019-10-05 00:21:49 +0530

    lombok added in model classes

--- src/main/java/com/arjunkumar/cricketapi/models/Match.java
+++ src/main/java/com/arjunkumar/cricketapi/models/Match.java
@@ -1,10 +1,15 @@
 package com.arjunkumar.cricketapi.models;
 
 
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Match {
-
-    // Fields ..........................................
 
     private String teamA = "";
     private String teamB = "";
@@ -13,67 +18,4 @@ public class Match {
     private String overA = "";
     private String overB = "";
 
-
-    // Constructors.....................................
-
-    public Match(){};
-
-    public Match(String teamA, String teamB, String scoreA, String scoreB, String overA, String overB) {
-        this.teamA = teamA;
-        this.teamB = teamB;
-        this.scoreA = scoreA;
-        this.scoreB = scoreB;
-        this.overA = overA;
-        this.overB = overB;
-    }
-
-    // Getters and Setters..............................
-
-    public String getTeamA() {
-        return teamA;
-    }
-
-    public void setTeamA(String teamA) {
-        this.teamA = teamA;
-    }
-
-    public String getTeamB() {
-        return teamB;
-    }
-
-    public void setTeamB(String teamB) {
-        this.teamB = teamB;
-    }
-
-    public String getScoreA() {
-        return scoreA;
-    }
-
-    public void setScoreA(String scoreA) {
-        this.scoreA = scoreA;
-    }
-
-    public String getScoreB() {
-        return scoreB;
-    }
-
-    public void setScoreB(String scoreB) {
-        this.scoreB = scoreB;
-    }
-
-    public String getOverA() {
-        return overA;
-    }
-
-    public void setOverA(String overA) {
-        this.overA = overA;
-    }
-
-    public String getOverB() {
-        return overB;
-    }
-
-    public void setOverB(String overB) {
-        this.overB = overB;
-    }
 }
